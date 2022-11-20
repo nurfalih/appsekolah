56e28ff8c65c485de4289b7c06ae26db58680ca3
nurfalih <118666029+nurfalih@users.noreply.github.com>
2022-11-20 19:34:36 +0700

    Add files via upload

--- AppSekolah.py
+++ AppSekolah.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from PIL import Image
+
 
 st.write("""
 # KUMPULAN SEKOLAH TERBAIK SMP DAN SMA
@@ -7,4 +7,7 @@ Website by Nurfalih and Hosting by Streamlit
 """)
 
 st.subheader("Sekolah Terbaik Saat ini")
+from PIL import Image
 image = Image.open('SMAN1Tangerang.jpg')
+
+st.image(image, caption='Sekolah SMAN 1 Tangerang Selatan')
