bb5f41a3c386b5689eab3a9c4671354f59ba83a2
nurfalih <118666029+nurfalih@users.noreply.github.com>
2022-11-20 19:54:19 +0700

    Add files via upload

--- AppSekolah.py
+++ AppSekolah.py
@@ -7,7 +7,7 @@ Website by Nurfalih and Hosting by Streamlit
 """)
 
 st.subheader("Sekolah Terbaik Saat ini")
-if st.button("SMAN1 Tangerang Selatan")
+if st.button('SMAN1 Tangerang Selatan')
     st.write('Pendaftaran penerimaan peserta didik baru tingkat SMA dan SMK di wilayah Provinsi Banten resmi diumumkan mulai Rabu, 15 Juni 2022.')
 B = st.button("SMAS Marsudirini")
 C = st.button("SMAN 1 Bekasi")
