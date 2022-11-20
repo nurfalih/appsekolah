8821315ecc00b83925a1556dfdb9b769e3ee419e
nurfalih <118666029+nurfalih@users.noreply.github.com>
2022-11-20 19:53:48 +0700

    Add files via upload

--- AppSekolah.py
+++ AppSekolah.py
@@ -8,7 +8,7 @@ Website by Nurfalih and Hosting by Streamlit
 
 st.subheader("Sekolah Terbaik Saat ini")
 if st.button("SMAN1 Tangerang Selatan")
-    st.write('merupakan salah satu Sekolah Menengah Atas Negeri yang ada di Provinsi Banten, Indonesia. Sama dengan SMA pada umumnya di Indonesia masa pendidikan sekolah di SMAN 1 Tangerang Selatan ditempuh dalam waktu tiga tahun pelajaran, mulai dari Kelas X sampai Kelas XII. Di Sekolah ini terdapat kelas bilingual. Pada tahun ajaran baru, di sekolah ini pun akan diterapkan sistem SKS.')
+    st.write('Pendaftaran penerimaan peserta didik baru tingkat SMA dan SMK di wilayah Provinsi Banten resmi diumumkan mulai Rabu, 15 Juni 2022.')
 B = st.button("SMAS Marsudirini")
 C = st.button("SMAN 1 Bekasi")
 D = st.button("SMAN 3 Bekasi")
